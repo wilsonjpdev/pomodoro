96a9a703809e76ba451ee6da94012cf2faf77679
wilsonjp <wilsonjp.dev@outlook.com>
2024-11-28 08:45:13 -0300

    criadas rotas iniciais

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/themes/global'
+import { Router } from './Router'
+import { BrowserRouter } from 'react-router-dom'
 
 export function App() {
 
   return (
-    <>
-        <h1>Teste</h1>
-    </>
+    <ThemeProvider theme={defaultTheme}>
+        <BrowserRouter>
+            <Router />
+        </BrowserRouter>
+        <GlobalStyle />
+    </ThemeProvider>
   )
 }
