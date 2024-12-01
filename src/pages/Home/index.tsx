fd4f748e28832036a4b277abcfaf0e543a88dbd4
wilsonjp <wilsonjp.dev@outlook.com>
2024-12-01 11:16:57 -0300

    estilizado pagina home

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,50 @@
+import { Play } from "phosphor-react";
+import { CountdownContainer, FormContainer, HomeContainer, MinutsAmountInput, Separator, StartCountdownButton, TaskInput } from "./style";
+
+export function Home() {
+    return(
+        <HomeContainer>
+            <form action="">
+                <FormContainer>
+                    <label htmlFor="task">Vou trabalhar em</label>
+                    <TaskInput
+                        id="task"
+                        type="text"
+                        placeholder="Dê um nome para o seu projeto"
+                        list="task-suggestions"
+                    />
+                    <datalist id="task-suggestions">
+                        <option value="Projeto 1" />
+                        <option value="Projeto 2" />
+                        <option value="Projeto 3" />
+                    </datalist>
+
+                    <label htmlFor="minutesAmount">Durante</label>
+                    <MinutsAmountInput
+                        id="minutesAmount"
+                        type="number"
+                        placeholder="00"
+                        step={5}
+                        min={5}
+                        max={60}
+                    />
+
+                    <span>minutos.</span>
+                </FormContainer>
+
+                <CountdownContainer>
+                    <span>0</span>
+                    <span>8</span>
+                    <Separator>:</Separator>
+                    <span>0</span>
+                    <span>0</span>
+                </CountdownContainer>
+
+                <StartCountdownButton type="submit">
+                    <Play size={24} />
+                    Começar
+                </StartCountdownButton>
+            </form>
+        </HomeContainer>
+    )
+}
